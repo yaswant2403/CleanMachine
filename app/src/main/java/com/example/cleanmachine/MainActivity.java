dc88a1dc44ff1705666bd76a1d09b812af0a0834
Yash <bobbyspamsemail@gmail.com>
2021-10-24 18:46:07 -0500

    Initial commit w/ Bottom Navigation Bar

---
+++ app/src/main/java/com/example/cleanmachine/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.cleanmachine;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new LoginFragment()).commit();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+
+    }
+
+    private final BottomNavigationView.OnItemSelectedListener navListener;
+    {
+        navListener = new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_scan:
+                        selectedFragment = new ScanFragment();
+                        break;
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    case R.id.nav_settings:
+                        selectedFragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+
+        };
+    }
+
+
+}
