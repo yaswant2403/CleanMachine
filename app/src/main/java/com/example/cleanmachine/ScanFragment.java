91dd9183aec70debf995fb292a4a02d0af8b836d
Fay <flocean682@gmail.com>
2021-12-02 19:57:21 -0600

    fay's algorithm 1st version

--- app/src/main/java/com/example/cleanmachine/ScanFragment.java
+++ app/src/main/java/com/example/cleanmachine/ScanFragment.java
@@ -55,30 +55,102 @@ public class ScanFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 //onclick store data
+                boolean landfill = true;
+                boolean paper = false;
+                boolean metal = false;
+                boolean plastic = false;
                 String input = description.getText().toString();
                 //use .split() to separate words
                 String[] words = input.split(",");
                 //FIRST ORGANIZATION
-                //category : cartons, glass, metal/foil, paper/cardboard, plastic
+                //category : metal/foil, paper/cardboard, plastic
                 for (int i = 0; i < words.length; i++) {
-
+                    if (words[i] == "paper" || words[i] == "cardboard") {
+                        paper = true;
+                        landfill = false;
+                    }
+                    if (words[i] == "plastic") {
+                        plastic = true;
+                        landfill = false;
+                    }
+                    if (words[i] == "metal" || words[i] == "aluminium" ) {
+                        metal = true;
+                        landfill = false;
+                    }
+                }
+                if (landfill) {
+                    description.setText("please put the item in the landfill, it cannot be sorted");
                 }
                 //SECOND ORGANIZATION
                 // if paper
                 // -> check for keywords "box" + "cardboard" = goes in separate cardboard dumpster
                 // make String array
+                if (paper) {
+                    boolean cardboard = false;
+                    boolean regular = false;
+                    boolean dirty = false;
+
+                    for (int j = 0; j < words.length; j++) {
+                        if (words[j] == "box" || words[j] == "cardboard" ) {
+                            cardboard = true;
+                        }
+                        if (words[j] == "book" || words[j] == "newspaper" || words[j] == "magazine"|| words[j] == "journal" || words[j] == "mail" || words[j] == "folder" ) {
+                            regular = true;
+                        }
+                        if (words[j] == "oily" || words[j] == "food") {
+                            dirty = true;
+                        }
+                    }
+                    if (dirty) {
+                        description.setText("please put the item in the landfill, it cannot be sorted");
+                    } else {
+                        if (cardboard) {
+                            description.setText("please put the item in a separate designated cardboard dumpster, not the recycling bins");
+                        }
+                        if (regular) {
+                            description.setText("you may put the object in the designated paper recycling bin");
+                        }
+                    }
+                }
+
                 // -> check for "book", "newspaper", "magazine", "journal", "mail", "envelope", "folder"
                 //if metal
+                if (metal) {
+                    boolean can = false;
+                    for (int x = 0; x < words.length; x++) {
+                        if (words[x] == "aluminium" || words[x] == "can" ) {
+                            can = true;
+                        }
+                    }
+                    if (can) {
+                        description.setText("you may put the object in the designated can/bottle recycling bin if it is rinsed and clean");
+                    } else {
+                        description.setText("please put the item in the landfill, it cannot be sorted");
+                    }
+                }
                 // check for "aluminium"
                 // else put in landfill
+                if (plastic) {
+                    boolean bottle = false;
+                    for (int x = 0; x < words.length; x++) {
+                        if (words[x] == "bottle") {
+                            bottle = true;
+                        } else {
+                            description.setText("please put the item in the landfill, it cannot be sorted");
+                        }
+                    }
+                    if (bottle) {
+                        description.setText("only type 1 or 2 bottles can be placed in the designated can/bottle bin, please check the type and place your clean bottle in the bin");
+                    } else {
+                        description.setText("please put the item in the landfill, it cannot be sorted");
+                    }
+                }
                 // if plastic
                 // -> check for "bottle"
                 // find some way to check if it is #1 or #2 type bottle
                 // else landfill
             }
         });
-        // tv remind user to clean all bottles or cans before dropping it in bin (metal+plastic)
-        // "exceptions" (automatic landfill words) -> "oily" (not cleanable usually) + "technology" (does not belong in any category) + "glass" + "tissue"
         return view;
     }
 }
